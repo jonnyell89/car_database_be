144836c7c95af3c3ef3d630be77f428ab014215a
jonnyell89 <jonnyell89@gmail.com>
2025-09-26 14:16:14 +0100

    logger added to CarDatabaseBeApplication, logging.level.root=DEBUG added to application.properties

--- src/main/java/com/packt/car_database_be/CarDatabaseBeApplication.java
+++ src/main/java/com/packt/car_database_be/CarDatabaseBeApplication.java
@@ -1,13 +1,19 @@
 package com.packt.car_database_be;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CarDatabaseBeApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(
+			CarDatabaseBeApplication.class
+	);
+
 	public static void main(String[] args) {
 		SpringApplication.run(CarDatabaseBeApplication.class, args);
+		logger.info("Application started: ");
 	}
-
 }
