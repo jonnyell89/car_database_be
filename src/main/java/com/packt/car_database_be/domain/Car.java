39737d3dec1757571d0475c29c4248fb8ea7d7a1
jonnyell89 <jonnyell89@gmail.com>
2025-09-26 19:42:43 +0100

    car entity class added, jpa dependency and h2 dependency added to build.gradle, application.properties updated

---
+++ src/main/java/com/packt/car_database_be/domain/Car.java
@@ -0,0 +1,96 @@
+package com.packt.car_database_be.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id; // Primary key.
+    private String brand, model, colour, registrationNumber; // Columns in database.
+    private int modelYear, price; // Columns in database.
+
+    public Car() {};
+
+    public Car(String brand, String model, String colour, String registrationNumber, int modelYear, int price) {
+        super();
+        this.brand = brand;
+        this.model = model;
+        this.colour = colour;
+        this.registrationNumber = registrationNumber;
+        this.modelYear = modelYear;
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public int getModelYear() {
+        return modelYear;
+    }
+
+    public void setModelYear(int modelYear) {
+        this.modelYear = modelYear;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", colour='" + colour + '\'' +
+                ", registrationNumber='" + registrationNumber + '\'' +
+                ", modelYear=" + modelYear +
+                ", price=" + price +
+                '}';
+    }
+}
