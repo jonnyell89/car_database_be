da8ee3c9247504866a02c7bf684799fcc2e70a17
jonnyell89 <jonnyell89@gmail.com>
2025-09-26 13:30:49 +0100

    initial commit - Spring Boot setup

---
+++ src/main/java/com/packt/car_database_be/CarDatabaseBeApplication.java
@@ -0,0 +1,13 @@
+package com.packt.car_database_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDatabaseBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarDatabaseBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/packt/car_database_be/CarDatabaseBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.packt.car_database_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarDatabaseBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
