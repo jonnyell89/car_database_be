73fc53c5cb7b80b1695c6011ef3b69709dca546e
jonnyell89 <jonnyell89@gmail.com>
2025-09-27 16:15:04 +0100

    carRepository implements CrudRepository, queries added

--- src/main/java/com/packt/car_database_be/domain/CarRepository.java
+++ src/main/java/com/packt/car_database_be/domain/CarRepository.java
@@ -1,5 +1,27 @@
 package com.packt.car_database_be.domain;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CarRepository extends CrudRepository<Car, Long> {}
+import java.util.List;
+
+public interface CarRepository extends CrudRepository<Car, Long> {
+    List<Car> findByBrand(String brand);
+    List<Car> findByModel(String model);
+    List<Car> findByColour(String colour);
+    List<Car> findByRegistrationNumber(String registrationNumber);
+    List<Car> findByModelYear(int modelYear);
+    List<Car> findByPrice(int price);
+
+    List<Car> findByBrandAndModel(String brand, String model);
+    List<Car> findByBrandOrColour(String brand, String colour);
+
+    List<Car> findByBrandOrderByModelYearAsc(String brand);
+
+//    Custom queries make your application less portable across different database systems.
+//    @Query("select c from Car c where c.brand = ?1")
+//    List<Car> findByBrand(String brand);
+//
+//    @Query("select c from Car c where c.brand like %?1")
+//    List<Car> findByBrandEndsWith(String brand);
+}
