7bf2cd33215758c17fd8069e02532baef7faf1b7
jonnyell89 <jonnyell89@gmail.com>
2025-09-30 16:04:05 +0100

    OpenAPI dependency added to build.gradle, OpenApiConfig configuration class added, RepositoryRestResource annotation added to CarRepository interface, Param annotation added to query parameters

---
+++ src/main/java/com/packt/car_database_be/OpenApiConfig.java
@@ -0,0 +1,15 @@
+package com.packt.car_database_be;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI carDatabaseBeOpenAPI() {
+        return new OpenAPI().info(new Info().title("Car REST API").description("Car Database").version("1.0"));
+    }
+}
--- src/main/java/com/packt/car_database_be/domain/CarRepository.java
+++ src/main/java/com/packt/car_database_be/domain/CarRepository.java
@@ -1,21 +1,24 @@
 package com.packt.car_database_be.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource
 public interface CarRepository extends CrudRepository<Car, Long> {
-    List<Car> findByBrand(String brand);
-    List<Car> findByModel(String model);
-    List<Car> findByColour(String colour);
-    List<Car> findByRegistrationNumber(String registrationNumber);
-    List<Car> findByModelYear(int modelYear);
-    List<Car> findByPrice(int price);
+    List<Car> findByBrand(@Param("brand") String brand);
+    List<Car> findByModel(@Param("model") String model);
+    List<Car> findByColour(@Param("colour") String colour);
+    List<Car> findByRegistrationNumber(@Param("registrationNumber") String registrationNumber);
+    List<Car> findByModelYear(@Param("modelYear") int modelYear);
+    List<Car> findByPrice(@Param("price") int price);
 
-    List<Car> findByBrandAndModel(String brand, String model);
-    List<Car> findByBrandOrColour(String brand, String colour);
+    List<Car> findByBrandAndModel(@Param("brand") String brand, @Param("model") String model);
+    List<Car> findByBrandOrColour(@Param("brand") String brand, @Param("colour") String colour);
 
-    List<Car> findByBrandOrderByModelYearAsc(String brand);
+    List<Car> findByBrandOrderByModelYearAsc(@Param("brand") String brand);
 
 //    Custom queries make your application less portable across different database systems.
 //    @Query("select c from Car c where c.brand = ?1")
